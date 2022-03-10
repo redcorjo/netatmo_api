00c2b10871266b176264b192e490e9c22a6da77c
redcorjo <jordipromotions@gmail.com>
2022-03-10 22:12:38 +0100

    changes

--- src/netatmo_api/netatmo_api.py
+++ src/netatmo_api/netatmo_api.py
@@ -12,15 +12,17 @@ logger = logging.getLogger(__name__)
 class Netatmo_API():
 
     endpoint = "https://api.netatmo.com"
+    home_id = None
     token = None
 
-    def __init__(self, client_id, client_secret, username, password, home_id, endpoint="https://api.netatmo.com"):
+    def __init__(self, client_id, client_secret, username, password, home_id: str = None, endpoint: str ="https://api.netatmo.com"):
         self.endpoint = endpoint
         self.client_id = client_id
         self.client_secret = client_secret
         self.username = username
         self.password = password
-        self.home_id = home_id
+        if home_id != None:
+            self.home_id = home_id
 
     def get_token(self):
         token = None
@@ -39,11 +41,128 @@ class Netatmo_API():
         self.token = token
         return token
 
+    def homesdata(self, home_id: str = None,  gateways_types: list = None):
+        endpoint = f"{self.endpoint}/api/homesdata"
+        parameters = {
+        }
+        if home_id != None:
+            parameters["home_id"] = home_id
+        elif self.home_id != None:
+            parameters["home_id"] = self.home_id
+        if gateways_types != None:
+            parameters["gateways_types"] = gateways_types
+        if self.token == None:
+            self.get_token()
+        headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer " + self.token
+        }
+        response = requests.get(endpoint, params=parameters, headers=headers)
+        return response
+
+    def homestatus(self, home_id: str,  device_types: list = None):
+        endpoint = f"{self.endpoint}/api/homestatus"
+        parameters = {
+        }
+        if home_id != None:
+            parameters["home_id"] = home_id
+        else:
+            parameters["home_id"] = self.home_id
+        if gateways_types != None:
+            parameters["gateways_types"] = gateways_types
+        if self.token == None:
+            self.get_token()
+        headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer " + self.token
+        }
+        response = requests.get(endpoint, params=parameters, headers=headers)
+        return response
+
+    # # TODO: Pending
+    # def getroommeasure(self):
+    #     endpoint = f"{self.endpoint}/api/getroommeasure"
+    #     parameters = {
+    #     }
+    #     if self.token == None:
+    #         self.get_token()
+    #     headers = {
+    #         "accept": "application/json",
+    #         "Authorization": "Bearer " + self.token
+    #     }
+    #     response = requests.get(endpoint, params=parameters, headers=headers)
+    #     return response
+
+    # # TODO: Pending
+    # def setroomthermpoint(self, mode="away"):
+    #     endpoint = f"{self.endpoint}/api/setroomthermpoint"
+    #     parameters = {
+    #         "home_id": self.home_id,
+    #         "mode": mode
+    #     }
+    #     if self.token == None:
+    #         self.get_token()
+    #     headers = {
+    #         "accept": "application/json",
+    #         "Authorization": "Bearer " + self.token
+    #     }
+    #     response = requests.post(endpoint, params=parameters, headers=headers)
+    #     return response
+
     def setthermmode(self, mode="away"):
         endpoint = f"{self.endpoint}/api/setthermmode"
         parameters = {
             "home_id": self.home_id,
             "mode": mode
+        }
+        if self.token == None:
+            self.get_token()
+        headers = {
+            "accept": "application/json",
+            "Authorization": "Bearer " + self.token
+        }
+        response = requests.post(endpoint, params=parameters, headers=headers)
+        return response
+
+    # # TODO: Pending
+    # def getmeasure(self):
+    #     endpoint = f"{self.endpoint}/api/getmmeasure"
+    #     parameters = {
+    #     }
+    #     if self.token == None:
+    #         self.get_token()
+    #     headers = {
+    #         "accept": "application/json",
+    #         "Authorization": "Bearer " + self.token
+    #     }
+    #     response = requests.get(endpoint, params=parameters, headers=headers)
+    #     return response
+
+    # # TODO: Pending
+    # def synchomeschedule(self, mode="away"):
+    #     endpoint = f"{self.endpoint}/api/synchomeschedule"
+    #     parameters = {
+    #         "home_id": self.home_id,
+    #         "mode": mode
+    #     }
+    #     if self.token == None:
+    #         self.get_token()
+    #     headers = {
+    #         "accept": "application/json",
+    #         "Authorization": "Bearer " + self.token
+    #     }
+    #     response = requests.post(endpoint, params=parameters, headers=headers)
+    #     return response
+
+    def switchhomeschedule(self, schedule_id: str, home_id: str = None):
+        endpoint = f"{self.endpoint}/api/switchhomeschedule"
+        if home_id != None:
+            my_home_id = home_id
+        else:
+            my_home_id = self.home_id
+        parameters = {
+            "home_id": self.my_home_id,
+            "schedule_id": schedule_id
         }
         if self.token == None:
             self.get_token()
