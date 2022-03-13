fa12c440d71b04360c8c7fee913beae50827637f
redcorjo <jordipromotions@gmail.com>
2022-03-13 21:54:48 +0100

    changes

---
+++ netatmo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+MYDIR=$(pwd)
+APPDIR=$(dirname $0)
+cd ${APPDIR}
+if ! test -d .venv
+then
+    echo creating virtual env
+    python3 -m venv .venv
+    . .venv/bin/activate
+    .venv/bin/python3 -m pip install -r requirements.txt
+else
+    . .venv/bin/activate
+fi
+
+( cd src ; ../.venv/bin/python3 netatmo.py --daemon )
+
+cd ${MYDIR}
+
+exit 0
