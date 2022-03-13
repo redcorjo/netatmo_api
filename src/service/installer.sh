f74a85ee9f2751cf99e4407d92548399acc831e7
redcorjo <jordipromotions@gmail.com>
2022-03-13 22:14:02 +0100

    changes

--- src/service/installer.sh
+++ src/service/installer.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 
-echo "Execute as pi user"
+create_service()
+{
+cat <<EOF>/tmp/netatmo.service
+[Unit]
+Description=Netatmo
+After=multi-user.target
 
-sudo cp netatmo.service /lib/systemd/system/netatmo.service
-test -d /home/pi/netatmo || mkdir /home/pi/netatmo
-cp -R ../../* /home/pi/netatmo
-sudo pip3 install -r /home/pi/netatmo/requirements.txt
-chmod 755 /home/pi/netatmo/src/netatmo.py
+[Service]
+Type=simple
+User=${MYUSER}
+WorkingDirectory=${WORKDIR}/
+ExecStart=${WORKDIR}/netatmo.sh
+Restart=on-abort
+
+[Install]
+WantedBy=multi-user.target
+EOF
+}
+
+echo "Execute as pi user"
+MYDIR=$(pwd)
+APPDIR=$(dirname $0)
+WORKDIR=$(cd ${APPDIR}; cd ../.. ; pwd)
+MYUSER=$(whoami)
+create_service
+sudo mv /tmp/netatmo.service /lib/systemd/system/netatmo.service
 sudo systemctl daemon-reload
 sudo systemctl enable netatmo.service
 sudo systemctl start netatmo.service
+exit
