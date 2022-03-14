d6a53d70499ba17ecd254e9a65a65c93acdd6ff2
redcorjo <jordipromotions@gmail.com>
2022-03-14 10:31:30 +0100

    changes

--- src/netatmo.py
+++ src/netatmo.py
@@ -10,6 +10,7 @@ import datetime
 import argparse
 from enum import Enum
 from apscheduler.schedulers.background import BackgroundScheduler
+from jinja2 import Template
 from netatmo_api import Netatmo_API
 from mqtt import MQTT
 
@@ -25,9 +26,7 @@ class MyNetatmo():
     mqtt = None
     scheduler = None
     all_data = { 
-            "home": {},
-            "room": {},
-            "module": {}
+            "homes": [],
         }
 
     def __init__(self, settings_file: str = None):
@@ -137,23 +136,22 @@ class MyNetatmo():
         netatmo = self.get_netatmo_session()
         homesdata_response = netatmo.homesdata()
         all_data = { 
-            "home": {},
-            "room": {},
-            "module": {}
+            "homes": [],
         }
         for homedata in homesdata_response["body"]["homes"]:
             my_home_id = homedata["id"]
-            all_data["home"][my_home_id] = homedata
+            all_data["homes"].append(homedata)
             self.mqtt.send_message(payload=homedata, item=my_home_id)
             homestatus_response = netatmo.homestatus(home_id=my_home_id)
             for room in homestatus_response["body"]["home"]["rooms"]:
                 item = room["id"]
-                all_data["room"][item] = room
                 self.mqtt.send_message(payload=room, item=item)
             for module in homestatus_response["body"]["home"]["modules"]:
                 item = module["id"]
-                all_data["module"][item] = module
                 self.mqtt.send_message(payload=module, item=item)
+        all_data["broker"] = self.broker
+        all_data["port"] = self.port
+        all_data["topic"] = self.topic
         self.all_data = all_data
         return None
 
@@ -164,7 +162,55 @@ class MyNetatmo():
 
     def create_openhab_template(self):
         logger.info("Create opehab template file")
-        return None
+        template_things = """
+        Bridge mqtt:broker:mosquitto [ host="{{broker}}", port={{port}}, secure=false ]
+{
+    // Homes
+    {% for my_home in homes -%}
+        Thing mqtt:topic:netatmo2mqtthome-{{my_home.id}} "netatmo2mqtt home {{my_home.id}}" {
+        Channels:
+            Type string   : id "netatmo2mqtt {{my_home.name}} id" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.id"]
+            Type string   : name "netatmo2mqtt {{my_home.name}} name" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.name"]
+            //Type string   : altitude "netatmo2mqtt {{my_home.name}} altitude" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.altitude"]
+            //Type location : coordinates "netatmo2mqtt {{my_home.name}} coordinates" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.coordinates"]
+            Type string   : country "netatmo2mqtt {{my_home.name}} country" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.country"]
+            Type string   : timezone "netatmo2mqtt {{my_home.name}} timezone" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.timezone"]
+            Type string   : temperature_control_mode "netatmo2mqtt {{my_home.name}} temperature_control_mode" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.temperature_control_mode"]
+            Type string   : therm_mode "netatmo2mqtt {{my_home.name}} therm_mode" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.therm_mode"]
+            Type string   : therm_setpoint_default_duration "netatmo2mqtt {{my_home.name}} therm_setpoint_default_duration" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.therm_setpoint_default_duration"]
+            Type string   : cooling_mode "netatmo2mqtt {{my_home.name}} cooling_mode" [ stateTopic="{{topic}}/{{my_home.id}}/state", transformationPattern="JSONPATH:.cooling_mode"]
+        }
+
+    // Rooms
+    {%for room in my_home.rooms -%}
+        Thing mqtt:topic:netatmo2mqttroom-{{room.id}} "netatmo2mqtt room {{room.id}}" {
+        Channels:
+            Type string   : id "netatmo2mqtt room {{room.name}} id" [ stateTopic="{{topic}}/{{room.id}}/state", transformationPattern="JSONPATH:.id"]
+            Type string   : name "netatmo2mqtt room {{room.name}} name" [ stateTopic="{{topic}}/{{room.name}}/state", transformationPattern="JSONPATH:.name"]
+            Type string   : type "netatmo2mqtt room {{room.name}} type" [ stateTopic="{{topic}}/{{room.type}}/state", transformationPattern="JSONPATH:.type"]
+        }
+    {% endfor %}
+
+    // Modules
+    {%for module in my_home.modules -%}
+        Thing mqtt:topic:netatmo2mqttmodule-{{my_home.id}} "netatmo2mqtt module {{my_home.id}}" {
+        Channels:
+            Type string     : id "netatmo2mqtt module {{module.name}} id"                   [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.id"]
+            Type string     : type "netatmo2mqtt module {{module.name}} type"               [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.type"]
+            Type string     : name "netatmo2mqtt module {{module.name}} name"               [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.name"]
+            //Type string     : setup_date "netatmo2mqtt module {{module.name}} setup_date"   [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.setup_date"]
+            Type string     : room_id "netatmo2mqtt module {{module.name}} room_id"         [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.room_id"]
+            Type string     : bridge "netatmo2mqtt module {{module.name}} bridge"           [ stateTopic="{{topic}}/{{module.id}}/state", transformationPattern="JSONPATH:.bridge"]
+        }
+    {% endfor %}
+    {% endfor %}
+}
+        """
+        template = Template(template_things)
+        data_things = template.render(self.all_data)
+        with open("netatmo.things", "w") as my_file:
+            my_file.write(data_things)
+        return data
 
 def get_flags():
     parser = argparse.ArgumentParser()
